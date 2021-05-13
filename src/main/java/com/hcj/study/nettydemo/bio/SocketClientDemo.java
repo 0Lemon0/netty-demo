29e8a2da8089d58772c5fbde78910cc6bcc302a5
hucj <hucj1102@thundersoft.com>
2021-05-13 16:33:11 +0800

    资源管理改用try-with-resource,简化代码

--- src/main/java/com/hcj/study/nettydemo/bio/SocketClientDemo.java
+++ src/main/java/com/hcj/study/nettydemo/bio/SocketClientDemo.java
@@ -4,7 +4,6 @@ import cn.hutool.core.net.NetUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
-import java.net.BindException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -15,74 +14,40 @@ import java.net.Socket;
  * @date 2021年04月22日 16:29
  */
 @Slf4j
-public class SocketClientDemo{
+public class SocketClientDemo {
     private static String ORDER_CLOSE = "CLOSE";
     private static Socket socket;
     private static int localPort = NetUtil.getUsableLocalPort();
-    private static DataInputStream input;
-    private static DataOutputStream out;
 
     static {
         try {
             socket = new Socket();
             //固定socket本地端口
-            socket.bind(new InetSocketAddress("127.0.0.1",localPort));
-            socket.connect(new InetSocketAddress("127.0.0.1",8010));
+            socket.bind(new InetSocketAddress("127.0.0.1", localPort));
+            socket.connect(new InetSocketAddress("127.0.0.1", 8010));
         } catch (IOException e) {
-            log.info("socket创建失败:{}",e.getMessage(),e);
+            log.info("socket创建失败:{}", e.getMessage(), e);
         }
     }
 
     public static void main(String[] args) {
-        while(true){
-            try {
-                //读取服务器端数据
-                input = new DataInputStream(socket.getInputStream());
-                //向服务器端发送数据
-                out = new DataOutputStream(socket.getOutputStream());
+        try (//读取服务器端数据
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            //向服务器端发送数据
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+            while (true) {
                 log.info("请输入指令:\t");
                 String order = new BufferedReader(new InputStreamReader(System.in)).readLine();
                 //String order = "CURRENT_TIME";
                 out.writeUTF(order);
-                if(ORDER_CLOSE.equalsIgnoreCase(order)){
-                    close();
+                if (ORDER_CLOSE.equalsIgnoreCase(order)) {
                     break;
                 }
                 //input.readUTF()阻塞至服务端响应
-                log.info("服务端响应:{}",input.readUTF());
-            } catch(IOException e){
-                log.error("ERROR:{}",e.getMessage(),e);
-                close();
-                break;
+                log.info("服务端响应:{}", input.readUTF());
             }
-        }
-    }
-
-    /**
-     * 关闭资源
-     */
-    private static void close() {
-        if(socket!=null){
-            try {
-                socket.close();
-            } catch (IOException e) {
-                log.error("socket close error:{}",e.getMessage(),e);
-            }
-        }
-        if(input!=null){
-            try {
-                input.close();
-            } catch (IOException e) {
-                log.error("inputSteam close error:{}",e.getMessage(),e);
-            }
-        }
-        if(out!=null){
-            try {
-                out.flush();
-                out.close();
-            } catch (IOException e) {
-                log.error("outputSteam close error:{}",e.getMessage(),e);
-            }
+        } catch (IOException e) {
+            log.error("ERROR:{}", e.getMessage(), e);
         }
     }
 }
--- src/main/java/com/hcj/study/nettydemo/bio/SocketServerThread.java
+++ src/main/java/com/hcj/study/nettydemo/bio/SocketServerThread.java
@@ -5,9 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.Socket;
-import java.net.SocketAddress;
 
 /**
  * socket服务端处理线程
@@ -20,10 +18,7 @@ public class SocketServerThread implements Runnable{
     private static String ORDER_CLOSE = "CLOSE";
     private static String VALID_ORDER = "CURRENT_TIME";
     private static String INVALID_ORDER_TIPS = "invalid order";
-
     private Socket socket;
-    private DataInputStream input;
-    private DataOutputStream out;
 
 
     public SocketServerThread(Socket socket) {
@@ -32,60 +27,24 @@ public class SocketServerThread implements Runnable{
 
     @Override
     public void run() {
-        while(true){
-            if(socket.isClosed()){
-                break;
-            }
-            //读取服务器端数据
-            //向服务器端发送数据
-            try {
-                input = new DataInputStream(socket.getInputStream());
-                out = new DataOutputStream(socket.getOutputStream());
+        try (//读取服务器端数据
+             //向服务器端发送数据
+             DataInputStream input = new DataInputStream(socket.getInputStream());
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream())){
+            while(true){
                 //读取客户端输入的指令,input.readUTF()阻塞至客户端请求
                 String order = input.readUTF();
                 String response;
                 if(ORDER_CLOSE.equalsIgnoreCase(order)){
-                    close();
                     break;
                 }
                 log.info("input order is {}",order);
                 response = VALID_ORDER.equalsIgnoreCase(order)? DateUtil.now():INVALID_ORDER_TIPS;
                 out.writeUTF(response);
-            } catch (IOException e) {
-                log.error("socket[{}] io error:{}",socket.getRemoteSocketAddress(),e.getMessage(),e);
-                close();
-                break;
             }
+        } catch (Exception e) {
+            log.error("socket[{}] io error:{}",socket.getRemoteSocketAddress(),e.getMessage(),e);
         }
-    }
-
-    /**
-     * 关闭资源
-     */
-    private void close() {
-        SocketAddress remoteAddress = socket.getRemoteSocketAddress();
-        if(socket!=null){
-            try {
-                socket.close();
-            } catch (IOException e) {
-                log.error("socket close error:{}",e.getMessage(),e);
-            }
-        }
-        if(input!=null){
-            try {
-                input.close();
-            } catch (IOException e) {
-                log.error("inputSteam close error:{}",e.getMessage(),e);
-            }
-        }
-        if(out!=null){
-            try {
-                out.flush();
-                out.close();
-            } catch (IOException e) {
-                log.error("outputSteam close error:{}",e.getMessage(),e);
-            }
-        }
-        log.info("connection closed:[{}]",remoteAddress);
+        log.info("connection close:{}",socket.getRemoteSocketAddress());
     }
 }
